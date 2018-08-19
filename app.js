39ee40e5bab02dffa48993c32cb2ddb932d46777
unknown <gos360@163.com>
2018-08-19 17:20:48 +0800

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const Koa = require('koa')
+const sha1 = require('sha1')
+const config = {
+    wechat: {
+        appID: '',
+        appSecret: '',
+        token: ''
+    }
+}
+const app = new Koa()
+
+app.use(async (ctx, next) =>{
+    const {signature, timestamp, nonce, echostr} = ctx.query 
+    const token = config.wechat.token
+    let str = [token, timestamp, nonce].sort().join('')
+    const sha = sha1(str)
+// 在自己的服务器，已经作过校验了
+    if (sha === signature) {
+        ctx.body = echostr
+    }else {
+        ctx.body = "wrong"
+    }
+})
+app.listen(3001)
+console.log('listen: '+ 3001);
