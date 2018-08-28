62a3b7259884fadf449694ea20841511be8a8c7f
unknown <gos360@163.com>
2018-08-28 18:02:29 +0800

    完成微信接入认证

--- wechat-lib/middleware.js
+++ wechat-lib/middleware.js
@@ -1,15 +1,25 @@
 const sha1 = require('sha1')
-module.exports = (config) => {
-    return async (ctx, next) => {
-        const { signature, timestamp, nonce, echostr } = ctx.query
-        const token = config.wechat.token
-        let str = [token, timestamp, nonce].sort().join('')
-        const sha = sha1(str)
-        // 在自己的服务器，已经作过校验了
-        if (sha === signature) {
-            ctx.body = echostr
-        } else {
-            ctx.body = "wrong"
-        }
+module.exports = config => {
+  return async (ctx, next) => {
+    const { signature, timestamp, nonce, echostr } = ctx.query
+    const token = config.wechat.token
+    let str = [token, timestamp, nonce].sort().join('')
+    const sha = sha1(str)
+    // 在自己的服务器，已经作过校验了
+    // console.log(signature)
+    // console.log(sha)
+    if (ctx.method === 'GET') {
+      if (sha === signature) {
+        ctx.body = echostr
+      } else {
+        ctx.body = 'Failed'
+      }
+    } else if (ctx.method === 'POST') {
+      if (sha !== signature) {
+        ctx.body = 'Failed'
+      } else {
+        ctx.body = 'wecome to la la la'
+      }
     }
+  }
 }
